dcd36641c4f4093e48be4b390ee84621dc45eb10
Kalib (Owner) <132502517+ConnellProducts@users.noreply.github.com>
2023-06-02 19:20:38 -0400

    Added New File! [NPCAnims.lua]
    hehehawhaw

---
+++ NPCAnims.lua
@@ -0,0 +1,46 @@
+local module = {
+	["Hollow"] = {
+		Action = {
+			["LMB"] = 13631503992;
+			["Spin"] = 13631505234;
+			["Rift Slash"] = 13631506589;
+			["Teleport Smash"] = 13631507763;
+			["Summon Skeletons"] = 13631508816;
+		};
+	};
+	["ShadowClone"] = {
+		Action = {
+			["LMB"] = 13631533410;
+		};
+	};
+	["Skeleton NPC"] = {
+		Action = {
+			["Hit"] = 13631535664;
+			["Headbutt"] = 13631536900;
+			["ArmHit"] = 13631549523;
+		};
+	};
+	["Bike"] = {
+		Idle = {
+			["Idle"] = 13631559672;
+		};
+		Movement = {
+			["Jump"] = 13631560690;
+		};
+		Action = {
+			["Summon"] = 13631561788;
+			["Desummon"] = 13631562826;
+			["Movement"] = 13631563718;
+		};
+	};
+	["Tatoo You"] = {
+		Action = {
+			["Hide"] = 13631565015;
+			["HideRecieve"] = 13631566200;
+			["Exit"] = 1;
+			["GunShot"] = 1;
+		};
+	}
+}
+
+return module
