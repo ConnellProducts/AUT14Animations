8dd46a9ca7c65cd3493533f037ddaf155ba21efa
Kalib (Owner) <132502517+ConnellProducts@users.noreply.github.com>
2023-06-02 23:48:15 -0400

    New File! [NubAnims.lua]

---
+++ NubAnims.lua
@@ -0,0 +1,264 @@
+local module = {
+	["Yasuo"] = {
+		Action = {
+			["LMB-1"] = 13631818310;
+			["LMB-2"] = 13631819598;
+			["LMB-3"] = 13631822072;
+			["Steel Tempest"] = 13631823288;
+			["Steel Tornado"] = 13631824535;
+			["Sweeping Blade"] = 13631825718;
+			["Gathering Storm"] = 13631826599;
+			["Galeforce"] = 13631827622;
+			["Barrage"] = 13631830695;
+			["Windwall"] = 13631832032;
+			["Ult Startup"] = 13631833259;
+			["Ult Slash"] = 13631834747;
+			["Call Of The Wind"] = 13631835937;
+			["Ult Beaten"] = 13631837171;
+			["Ult Cam"] = 13631838072;
+			["Grab Startup"] = 13631839043;
+			["Grab"] = 13631840167;
+			["Being Grabbed"] = 13631841392;
+			["Grab Cam"] = 13631842735;
+			["PoseTransition"] = 13631843680;
+			["PoseIdle"] = 13631844751;
+		};
+		Movement = {
+			["Walk"] = 13631846107;
+			["Run"] = 13631847223;
+		};
+		Idle = {
+			["Idle"] = 13631848584;	
+		};
+	};
+	["Yone"] = {
+		Action = {
+			["LMB-1"] = 13631849739;
+			["LMB-2"] = 13631851076;
+			["LMB-3"] = 13631853082;
+			["Soul LMB-1"] = 13631854212;
+			["Soul LMB-2"] = 13631855538;
+			["Soul LMB-3"] = 13631856326;
+			["Mortal Steel"] = 13631857611;
+			["Mortal Dash"] = 13631858623;
+			["Spirit Cleave"] = 13631859685;
+			["Soul Unbound"] = 13631860834;
+			["Soul Return"] = 13631862773;
+			["Leg Slice"] = 13631863633;
+			["Fate Sealed"] = 13631864821;
+			["Barrage"] = 13631866007;
+			["PoseTransition"] = 13631866913;
+			["PoseIdle"] = 13631867783;
+		};
+		Movement = {
+			["Walk"] = 13631868851;
+			["Run"] = 13631869913;
+			["SoulWalk"] = 13631870854;
+			["SoulRun"] = 13631871668;
+		};
+		Idle = {
+			["Idle"] = 13631872517;	
+			["SoulIdle"] = 13631873775;	
+		};
+	};
+	["Locke"] = {
+		Action = {
+			["Summon"] = 13631875906;
+			["LMB-1"] = 13631877148;
+			["LMB-2"] = 13631878260;
+			["LMB-3"] = 13631880071;
+			["Being Grabbed"] = 13631881210;
+			["Grab Windup"] = 13631882281;
+			["Grab Slam"] = 13631883249;
+			["Kingslayer"] = 13631884342;
+			["PoseTransitionStand"] = 13631885404;
+			["PoseTransitionUser"] = 13631886392;
+			["PoseIdle"] = 13631887821;
+		};
+	};
+	["STWR"] = {
+		Action = {
+			["Summon"] = 13631899412;
+			["LMB-1"] = 13631891510;
+			["LMB-2"] = 13631900757;
+			["LMB-3"] = 13631893086;
+			["LMB-4"] = 13631894624;
+			["Arrow Shoot"] = 13631901926;
+			["Cursed Punches"] = 13631902920;
+			["HeavyPunch"] = 13631904460;
+			["Arrow Inversion"] = 13631905892;
+			["Pose"] = 13631906790;
+			["Timestop"] = 13631907893;
+			["PoseTransition"] = 13631908995;
+			["KnifeThrow"] = 13631910499
+		};
+		Movement = {
+			["Walk"] = 13631911624;
+			["Run"] = 13631912788;
+			["SummonedRun"] = 13631913594;
+			["SummonedWalk"] = 13631914957;
+		};
+		Idle = {
+			["Idle"] = 13631916474;
+		};
+	};
+	["SPR"] =  {
+		Action = {
+			["Summon"] = 13631918032;
+			["LMB-1"] = 13631891510;
+			["LMB-2"] = 13631919047;
+			["LMB-3"] = 13631893086;
+			["LMB-4"] = 13631894624;
+			["GroundSlam"] = 13631920296;
+			["Cursed Punches"] = 13631921385;
+			["Arrow Judgement"] = 13631922555;
+			["Arrow Prison"] = 13631923700;
+			["Arrow Throw"] = 13631925000;
+			["Fly"] = 13631926106;
+			["Star Breaker"] = 13631927041;
+			["Timestop"] = 13631928023;
+			["Double Punch"] = 13631928718;
+			["Zoom"] = 13631929551;
+			["LoopedPose"] = 13631930862;
+			["PoseTransition"] = 13631932541;
+		};
+		Movement = {
+			["Run"] = 13631934017;
+			["Walk"] = 13631935182;
+			["SummonedRun"] = 13631936299;
+			["SummonedWalk"] = 13631937419;
+		};
+		Idle = {
+			["Idle"] = 13631938572;
+		};
+	};
+	["Dawn"] = {
+		Action = {
+			["Summon"] = 13632051586;
+			["LMB-1"] = 13632627224;
+			["LMB-2"] = 13632629054;
+			["LMB-3"] = 13632630364;
+			["LMB-4"] = 13632631818;
+			["LMB-5"] = 13632633382;
+			["Strike-1"] = 13632635131;
+			["Strike-2"] = 13632640801;
+			["Strike-3"] = 13632641963;
+			["Whirling Inferno Startup"] = 13632644190;
+			["Whirling Inferno Loop"] = 13632645597;
+			["True Blaze Startup"] = 13632647092;
+			["True Blaze Loop"] = 13632666610;
+			["True Blaze Slam"] = 13632668037;
+			["Eruption"] = 13632669322;
+			["Counter Stance"] = 13632716499;
+			["Counter Attack"] = 13632717685;
+			["Enchant"] = 13632718904;
+			["Phoenix"] = 13632720389;
+		};
+		Movement = {
+			["Walk"] = 13632721887;
+			["Run"] = 13632723090;
+		};
+		Idle = {
+			["Idle"] = 13632724125;	
+		};
+	};
+	["Umbra"] = {
+		Action = {
+			["Summon"] = 13633319525;
+			["SovereignSummon"] = 13633338828;
+			["LMB-1"] = 13633339839;
+			["LMB-2"] = 13633341031;
+			["LMB-3"] = 13633342044;
+			["LMB-4"] = 13633342995;
+			["LMB-5"] = 13633343947;
+			["Slashes"] = 13633345385;
+			["Last Rites"] = 13633346567;
+			["Elegant Bladework"] = 13633347899;
+			["Counter Stance"] = 13633349171;
+			["Judgement Stance"] = 13633350558;
+			["Counter-1"] = 13633351810;
+			["Counter-2"] = 13633352686;
+			["Counter-3"] = 13633353462;
+			["Counter-4"] = 13633354454;
+			["Counter-5"] = 13633355398;
+			["Spin"] = 13633356428;
+			["Umbral Execution"] = 13633379402;
+			["HeavySlash"] = 13633381188;
+			["Lunge"] = 13633382316;
+			["Pin"] = 13633383501;
+			["Dash"] = 13633384655;
+			["Block"] = 13633386325;
+			["Pose"] = 13633387645;
+		};
+		Idle = {
+			["SummonedIdle"] = 13633389056;
+			["SovereignIdle"] = 13633389984;
+		};
+	};
+	["Xeno"] = {
+		Action = {
+			["Summon"] = 13633391378;
+			["LMB-1"] = 13633392464;
+			["LMB-2"] = 13633393434;
+			["LMB-3"] = 13633394332;
+			["Death Blow"] = 13633395646;
+			["Grab"] = 13633396574;
+			["Chops"] = 13633397417;
+			["Pose"] = 13633398343;
+		};
+		Idle = {
+			["Idle"] = 13633399458;
+		};
+	};
+	["Goku"] = {
+		Action = {
+			["LMB-1"] = 13633402461;
+			["LMB-2"] = 13633403275;
+			["LMB-3"] = 13633404379;
+			["LMB-4"] = 13633405374;
+			["Kick-1"] = 13633406235;
+			["Kick-2"] = 13633407764;
+			["Kick-3"] = 13633408960;
+			["Kick-down"] = 13633409960;
+			["Kick-left"] = 13633410826;
+			["Kick-right"]= 13633411824;
+			["KameStart"] = 13633412884;
+			["KameHold"] = 13633414125;
+			["Barrage"] = 13633415130;
+			["Feint"] = 13633416409;
+			["Meteor Finish"] = 13633417451;
+			["Genkidama"] = 13633418594;
+			["Dragon Fist"] = 13633419634;
+			["Blink"] = 13633420560;
+			["Instant Transmission"] = 13633421543;
+			["Startup Charge"] = 13633422463;
+			["Charging"] = 13633424022;
+			["SSJ-Transform"] = 13633424959;
+			["SSJ2-Transform"] = 13633424959;
+			["SSJ3-Transform"] = 13633426724;
+			["SSJ4-Transform"] = 13633427618;
+			["SSJG-Transform"] = 13633428540;
+			["SSJB-Transform"] = 13633428540;
+			["SSJBK-Transform"] = 13633429434;
+			["UI-Transform"]	= 13633430301;
+			["MUI-Transform"]	= 13633430301;
+		};
+		["Idle"] = {
+			["Idle"] = 13633431760;
+			["Base"] = 13633431760;
+			["SSJ"] = 13633433500;
+			["SSJ2"] = 13633435438;
+			["SSJ3"] = 13633436490;
+			["SSJ4"] = 13633437583;
+			["SSJG"] = 13633438534;
+			["SSJB"] = 13633439701;
+			["SSJBK"] = 13633439701;
+			["UI"] = 13633440917;
+			["MUI"] = 13633440917;
+		};
+	};
+}
+--module.CSPR = module.SPR
+module.HSTWR = module.STWR
+
+return module
