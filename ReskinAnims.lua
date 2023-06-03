7f3007e4d3c85d78fc2adfb72b78c0c21448d3b1
Kalib (Owner) <132502517+ConnellProducts@users.noreply.github.com>
2023-06-02 23:54:15 -0400

    New File! [ReskinAnims.lua]

---
+++ ReskinAnims.lua
@@ -0,0 +1,97 @@
+local module = {
+	["Elucidator & Dark Repulser"] = { -- Yone
+		Idle = {
+			["Idle"] = 13633455666;	
+		};
+	};
+	["Reckoner"] = { -- STW 
+		Action = {
+			["StandIdle"] = 13633457352;
+			["Summon"] = 13633458167;
+		};
+		Idle = {
+			["Idle"] = 13633459183; --SummonedIdle
+		};
+	};
+	-- Umbra | 46 --
+	["First Drifter"] = { 
+		Idle = {
+			["SummonedIdle"] = 13633460191; --SummonedIdle
+		};
+	};
+	["Ama No Murakumo"] = {  
+		Idle = {
+			["SummonedIdle"] = 13633461272;
+		};
+	};
+	-- The World | 4 --
+	["TW29"] = {
+		Idle = {
+			["Idle"] = 13633462440; 
+		};
+	};
+	["Kathastrophe"] = {
+		Action = {
+			["StandIdle"] = 13633463654;
+		};
+		Idle = {
+			["Idle"] = 13633464606; 
+		};
+	};
+	-- --
+	["20XX"] = { -- TWHV 
+		--[[ Not Positioned Correctly, unfortunate
+		Action = {
+			["StandIdle"] = 9836742374;
+		};
+		]]
+		Idle = {
+			["Idle"] = 13633466204; 
+		};
+	};
+	["Blessing"] = { -- TW:OH 
+		Idle = {
+			["Idle"] = 13633467620; 
+		};
+	};
+	["Noob Platinum"] = { -- Noob/TW:OH 
+		Action = {
+			["StandIdle"] = 13633468719;
+			["PoseIdle"] = 13633469750;
+		}
+	};
+	["Noob Rave"] = { -- DJNoob/TW:OH 
+		Action = {
+			["StandIdle"] = 13633470466;
+			["PoseIdle"] = 13633471467;
+			["PoseIdle2"] = 13633472844;
+		}
+	};
+	["Fabled Platinum"] = { -- TW:OH 
+		Action = {
+			["StandIdle"] = 13633473999;
+			["PoseIdle"] = 13633474952;
+			["PoseIdle2"] = 13633475754;
+		}
+	};
+	["Risen Sun"] = { -- Nocturnus 
+		--[[ Not Positioned Correctly, unfortunate
+		Action = {
+			["StandIdle"] = 9839224158;
+		};
+		]]
+		Idle = {
+			["Idle"] = 13633477286; 
+		};
+	};
+	-- Casey | 8881 --
+	["Mjölnir"] = {
+		Action = {
+			["Bonk"] = 13633478685;
+			["Swing"] = 13633479610;
+			["Dash"] = 13633480384;
+		};
+	};
+}
+
+return module
